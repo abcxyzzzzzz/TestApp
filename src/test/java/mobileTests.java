2b77e07522724138a9c681b2b92600c7939f9b05
Tuyên <hmtuyen02@gmail.com>
2024-04-03 11:27:33 +0700

    Test app dau tien

--- src/test/java/mobileTests.java
+++ src/test/java/mobileTests.java
@@ -3,6 +3,8 @@ import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileBrowserType;
 import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.By;
+import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.MalformedURLException;
@@ -22,17 +24,47 @@ public class mobileTests {
     }
         public static void openCalculator() throws Exception {
             DesiredCapabilities cap = new DesiredCapabilities();
-            cap.setCapability("deviceName","testmapp");
-            cap.setCapability("udid","emulator-5554");
+            cap.setCapability("deviceName","testmobilee");
+            cap.setCapability("udid","192.168.61.101:5555");
             cap.setCapability("platformName","Android");
-            cap.setCapability("platformVersion","5.1.1");
-            cap.setCapability("appPackage","com.android.calculator2");
-            cap.setCapability("appActivity","com.android.calculator2.Calculator");
-
-
+            cap.setCapability("platformVersion","12.1");
+            cap.setCapability("appPackage","com.lcssoft.aloqtsc");
+            cap.setCapability("appActivity","com.lcssoft.aloqtsc.MainActivity");
             URL url = new URL("http://127.0.0.1:4723/wd/hub");
-            driver = new AppiumDriver<MobileElement>(url,cap);
+            driver = new AndroidDriver<MobileElement>(url,cap);
             System.out.println("Application Started...");
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            MobileElement skipButton = driver.findElement(By.xpath("//android.widget.Button[@content-desc=\"Skip\"]"));
+            skipButton.click();
+            driver.findElement(By.xpath("//android.widget.EditText")).click();
+            driver.findElement(By.xpath("//android.widget.EditText")).sendKeys("2837158888");
+            driver.findElement(By.xpath("//android.widget.Button[@content-desc=\"Next\"]")).click();
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            driver.findElement(By.xpath("//android.widget.Button[@content-desc=\"Next\"]")).click();
+            Thread.sleep(5000);
+            driver.findElement(By.xpath("//android.widget.FrameLayout[@resource-id=\"android:id/content\"]/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.widget.ImageView")).click();
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            driver.findElement(By.xpath("//android.widget.ScrollView/android.widget.EditText[1]")).click();
+            driver.findElement(By.xpath("//android.widget.ScrollView/android.widget.EditText[1]")).sendKeys("giúp tôi sữa chữa nhé");
+            driver.findElement(By.xpath("//android.widget.ScrollView/android.widget.EditText[2]")).click();
+            driver.findElement(By.xpath("//android.widget.ScrollView/android.widget.EditText[2]")).sendKeys("giúp tôi sữa chữa nhé");
+            driver.findElement(By.xpath("//android.view.View[@content-desc=\"Add image\"]")).click();
+            driver.findElement(By.xpath("//android.view.View[@content-desc=\"Chose image\"]")).click();
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            driver.findElement(By.id("com.android.permissioncontroller:id/permission_allow_button")).click();
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            driver.findElement(By.xpath("//android.widget.ImageView[@resource-id=\"com.android.documentsui:id/icon_thumb\"]")).click();
+            Thread.sleep(1000);
+            driver.findElement(By.xpath("//android.widget.Button[@content-desc=\"Create issue\"]")).click();
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            driver.findElement(By.xpath("//android.widget.Button[@content-desc=\"Close\"]")).click();
+
+
+
+
+
+
+
         }
 
 
